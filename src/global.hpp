d973f0a6760404452a8e080f3b3b54906e24b015
Dario <barone.dario97@gmail.com>
2023-01-23 15:16:02 +0100

    subtle edit to randomChoice function

--- src/global.hpp
+++ src/global.hpp
@@ -2,7 +2,10 @@
 #define global_h
 
 #include <fstream>
+#include <random>
 #include "distribution.hpp"
+
+extern std::mt19937_64 random_engine;
 
 extern UniformDistribution uniform_distribution;
 
--- src/random.hpp
+++ src/random.hpp
@@ -1,7 +1,7 @@
 #ifndef random_h
 #define random_h
 
-#include <iostream>
+#include <stdexcept>
 #include <vector>
 #include <random>
 #include <numeric>
@@ -11,16 +11,20 @@
 template<typename T>
 std::vector<T> randomChoice(const std::vector<T> & v, int N_extractions, std::vector<double> weights){
 	const int v_size = v.size();
-	if (N_extractions >= v_size){
-		std::cerr << "ERROR: too many extractions required, exceeding input vector" << std::endl;
-		return std::vector<T>();
+	if (N_extractions > v_size){
+		throw std::logic_error("randomChoice() cannot extract more elements than provided");
+	}
+	else if (N_extractions == v_size){
+		std::vector<T> extractions(v);
+		std::shuffle(extractions.begin(), extractions.end(), random_engine);
+		return extractions;
 	}
 	weights.resize(v_size);
 	std::vector<T> extractions(N_extractions);
 	double normalization(std::accumulate(weights.begin(), weights.end(), 0.));
 	for (auto i = 0; i < N_extractions; ++i){
-		double rnd(uniform_distribution() * normalization);
-		double cumulative_probability(0);
+		double rnd = uniform_distribution() * normalization;
+		double cumulative_probability = 0;
 		for (auto idx_extracted = 0; idx_extracted < v_size; ++idx_extracted){
 			cumulative_probability += weights[idx_extracted];
 			if (rnd < cumulative_probability){
@@ -38,9 +42,13 @@ std::vector<T> randomChoice(const std::vector<T> & v, int N_extractions, std::ve
 template<typename T>
 std::vector<T> randomChoice(std::vector<T> v, int N_extractions, bool replace=false){
 	int v_size = v.size();
-	if (!replace && (N_extractions>=v_size)){
-		std::cerr << "ERROR: too many extractions required, exceeding input vector" << std::endl;
-		return std::vector<T>();
+	if (!replace && (N_extractions>v_size)){
+		throw std::logic_error("randomChoice() cannot extract more elements than provided");
+	}
+	if (N_extractions == v_size){
+		std::vector<T> extractions(v);
+		std::shuffle(extractions.begin(), extractions.end(), random_engine);
+		return extractions;
 	}
 	std::vector<T> extractions(N_extractions);
 	for (auto i = 0; i < N_extractions; ++i){
