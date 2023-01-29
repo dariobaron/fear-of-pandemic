1d6c9d6c35e89935196aae755398ca0d8f66d7b4
Dario Barone <dbarone@sohrab001.lmp.ds.mpg.de>
2023-01-29 16:05:49 +0100

    simulations performed

--- run.sh
+++ run.sh
@@ -10,11 +10,18 @@ REACTIONTYPE=( 'soften' 'cut' )
 FEARDISTRIBUTION=( 'fixed' 'uni' 'asyLow' 'asyHigh' 'bi' 'corr' 'anticorr' )
 FEEDBACKTYPE=( 'none' 'short' 'long' 'shortlong' 'neighbours' 'all' )
 
+outputdir="${1/input/"output"}"
+outputdir="${outputdir%.*}"
+mkdir -p $outputdir
+
+outputfile="$outputdir/execution_log.txt"
+echo "" >$outputfile
+
 if [[ $# -eq 1 && -e $1 ]]; then
 	for reaction in "${REACTIONTYPE[@]}"; do
 		for fear in "${FEARDISTRIBUTION[@]}"; do
 			for feedback in "${FEEDBACKTYPE[@]}"; do
-				src/simulation.x $1 $reaction $fear $feedback
+					src/simulation.x $1 $reaction $fear $feedback 2>>$outputfile
 			done
 		done
 	done
