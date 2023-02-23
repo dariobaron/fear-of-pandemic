840d625e6ee30ab322a75ee5fb4c660b7a16e22c
Dario Barone <barone.dario97@gmail.com>
2023-02-23 14:35:27 +0100

    new simulations

--- run_on_cluster.sh
+++ run_on_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #$ -S /bin/bash
-#$ -N fop_l5
+#$ -N fop_l10
 #$ -q rostam.q
 #$ -cwd
 #$ -o /data.nst/dbarone/fop_log/
@@ -23,8 +23,9 @@ if [ -z ${SGE_TASK_ID+x} ]; then
 	echo "$(( n_r*n_d*n_f )) jobs are needed to parse the arguments"
 else 
 	echo "start simulation on cluster with SGE_TASK_ID=${SGE_TASK_ID}"; 
-	index_reaction=$(( (${SGE_TASK_ID}-1)/n_r/n_d ))
-	index_distribution=$(( (${SGE_TASK_ID}-1)/n_r%n_d ))
-	index_feedback=$(( (${SGE_TASK_ID}-1)%n_r ))
-	src/simulation.x 'input/bianconi-barabasi_10000_5/' ${REACTIONTYPE[index_reaction]} ${FEARDISTRIBUTION[index_distribution]} ${FEEDBACKTYPE[index_feedback]}
+	index_reaction=$(( (${SGE_TASK_ID}-1)/(n_d*n_f) ))
+	index_distribution=$(( (${SGE_TASK_ID}-1)/n_f%n_d ))
+	index_feedback=$(( (${SGE_TASK_ID}-1)%n_f ))
+	echo "src/simulation.x 'input/bianconi-barabasi_10000_10/' ${REACTIONTYPE[index_reaction]} ${FEARDISTRIBUTION[index_distribution]} ${FEEDBACKTYPE[index_feedback]}"
+	src/simulation.x 'input/bianconi-barabasi_10000_10/' ${REACTIONTYPE[index_reaction]} ${FEARDISTRIBUTION[index_distribution]} ${FEEDBACKTYPE[index_feedback]}
 fi
